07aaf9cb50b1c3f63b4940e7cbce2677c3ca8e8d
Taras Sidak <tarasidak1990@gmail.com>
2020-10-01 15:52:04 +0300

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const food = {
+  'Meat': {
+    'Pork': {},
+    'Beef': {},
+  },
+
+  'Fruit': {
+    'Red': {
+      'Cherry': {},
+      'Strawberry': {},
+    },
+    'Yellow': {
+      'Banana': {},
+      'Pineapple': {},
+    },
+
+  },
+};
+
+const tree = document.querySelector('#tree');
+
+function createTree(element, data) {
+  const ul = document.createElement('ul');
+
+  element.append(ul);
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.append(key);
+    ul.append(li);
+    createTree(li, data[key]);
+  }
+}
+
+createTree(tree, food);
